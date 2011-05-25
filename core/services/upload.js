cc2d664de2dacde52538db9e06efa9b5da34add5
Thomas Coats <thomas.coats@voodoolabs.net>
2011-05-26 08:00:39 +1200

    Stuff

--- core/services/upload.js
+++ core/services/upload.js
@@ -1,7 +1,6 @@
 var formidable = require('formidable');
 
-var progresses = {}
-var metadata   = {}
+var status = {};
 
 this.request = function (url, req, res) {
     var match = null;
@@ -10,6 +9,16 @@ this.request = function (url, req, res) {
     match = new RegExp('/upload/(.+)').exec(url.pathname);
     if (match && req.method.toLowerCase() == 'post') {
         var uuid = match[1];
+        status[uuid] = {
+            filename: '',
+            mime: '',
+            metadata: {},
+            progress: {
+                received: 0,
+                expected: 0,
+                percent: 0
+            }
+        };
         console.log("receiving upload: "+uuid+'\n');
         
         var form = new formidable.IncomingForm();
@@ -17,9 +26,12 @@ this.request = function (url, req, res) {
         form.keepExtensions = true;
         
         // keep track of progress.
-        form.addListener('progress', function(recvd, expected) {
-            progress = (recvd / expected * 100).toFixed(2);
-            progresses[uuid] = progress
+        form.addListener('progress', function(received, expected) {
+            status[uuid].progress = {
+                received: received,
+                expected: expected,
+                percent: (received / expected * 100).toFixed(2)
+            }
         });
         
         form.parse(req, function(error, fields, files) {
@@ -30,14 +42,11 @@ this.request = function (url, req, res) {
                 res.end();
                 return;
             }
-            var path = files['file']['path'];
-            var filename = files['file']['filename'];
-            var mime = files['file']['mime'];
-            res.writeHead(200, {'content-type': 'text/html'});
-            res.write('<textarea>');
-            res.write("upload complete.\n");
-            res.write(filename + ' landed safely at ' + path + '\n');
-            res.write('</textarea>')
+            status[uuid].savedfilename = files['file']['path'].substr(url.upload.length);
+            status[uuid].filename = files['file']['filename'];
+            status[uuid].mime = files['file']['mime'];
+            res.writeHead(200, {'content-type': 'text/plain'});
+            res.write('OK')
             res.end()
             console.log("finished upload: "+uuid+'\n');
         });
@@ -45,24 +54,36 @@ this.request = function (url, req, res) {
     }
     
     // (update) metadata
-    match = new RegExp('/update/(.+)').exec(url.pathname);
-    if (match && req.method.toLowerCase() == 'post') {
-        uuid = match[1]
-        var form = new formidable.IncomingForm();
-        form.addListener('field', function(name, value) {
-            console.log("fresh metadata for "+uuid+": "+name+" => "+value+"\n")
-            metadata[name] = value;
-        });
-        form.parse(req);
-        return true;
-    }
+    //match = new RegExp('/update/(.+)').exec(url.pathname);
+    //if (match && req.method.toLowerCase() == 'post') {
+    //    uuid = match[1]
+    //    var form = new formidable.IncomingForm();
+    //    form.addListener('field', function(name, value) {
+    //        console.log("fresh metadata for "+uuid+": "+name+" => "+value+"\n")
+    //        status[uuid].metadata[name] = value;
+    //    });
+    //    form.parse(req);
+    //    return true;
+    //}
     
     // respond to progress queries.
     match = new RegExp('/progress/(.+)').exec(url.pathname);
     if (match) {
         uuid = match[1];
         res.writeHead(200, {'content-type': 'application/json'});
-        res.write(JSON.stringify({'progress': progresses[uuid]}));
+        if (status[uuid])
+            res.write(JSON.stringify(status[uuid]));
+        else
+            res.write(JSON.stringify({
+                filename: '',
+                mime: '',
+                metadata: {},
+                progress: {
+                    received: 0,
+                    expected: 0,
+                    percent: 0
+                }
+            }));
         res.end();
         return true;
     }
