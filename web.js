b73a225fafd0ad1e6a6a058b9f89e34ea2434b93
Thomas Coats <github@voodoolabs.net>
2011-08-30 20:54:47 +1200

    url for redis

--- web.js
+++ web.js
@@ -1,10 +1,13 @@
 var express = require('express');
 var redis = require('redis-url');
-
 var app = express.createServer(express.logger());
 
+var redis_url = process.env.REDISTOGO_URL || 'localhost:6379';
+var rtg = require('url').parse(redis_url);
+
+
 app.get('/', function(request, response) {
-    var client = redis.createClient(process.env.REDISTOGO_URL);
+    var client = redis.createClient(rtg.port, rtg.hostname);
     client.set('foo', 'bar');
     client.get('foo', function(err, value) {
         response.send('foo is: ' + value);
