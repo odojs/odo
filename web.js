efa03b0d930620ec0d03f4d8430e97bdcb8dddd9
Thomas Coats <github@voodoolabs.net>
2011-08-30 20:51:13 +1200

    redis

--- web.js
+++ web.js
@@ -1,12 +1,17 @@
 var express = require('express');
+var redis = require('redis-url');
 
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World!');
+    var client = redis.createClient(process.env.REDISTOGO_URL);
+    client.set('foo', 'bar');
+    client.get('foo', function(err, value) {
+        response.send('foo is: ' + value);
+    });
 });
 
 var port = process.env.PORT || 3000;
 app.listen(port, function() {
-  console.log("Listening on " + port);
+    console.log("Listening on " + port);
 });
